d43712979e27bd10f5e01b55243df6b37f2e1fe3
mosharaf456 <mosharaf111hossain@gmail.com>
2024-06-29 02:02:20 +0600

    ref docerization done ci4

--- src/app/public/index.php
+++ src/app/public/index.php
@@ -1,4 +1,12 @@
 <?php
+/*
+ *---------------------------------------------------------------
+ * CORS HEADERS
+ *---------------------------------------------------------------
+ */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS, DELETE, PUT');
+header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
 
 /*
  *---------------------------------------------------------------
