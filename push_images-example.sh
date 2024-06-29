7279cac33d89732b960a4066284928a04b34c466
mosharaf456 <mosharaf111hossain@gmail.com>
2024-06-29 11:12:13 +0600

    ref docker hub push script added

---
+++ push_images-example.sh
@@ -0,0 +1,32 @@
+#!/bin/bash +x
+
+# Docker repository credentials
+DOCKER_USERNAME="abc"
+DOCKER_PASSWORD="12"
+
+# Log in to Docker repository
+echo ${DOCKER_PASSWORD} | docker login --username ${DOCKER_USERNAME} --password-stdin
+
+# Define image names and tags
+MYSQL_IMAGE_NAME="mysql-db-cus-image"
+MYSQL_TAG="v1.0.1"
+MYSQL_REPO="mosharafhossain/mysql-db-cus-image:latest"
+
+PHP_IMAGE_NAME="web-ci4-cus-image"
+PHP_TAG="v1.0.1"
+
+PHP_REPO="mosharafhossain/ci4-custom-web-img:v.1.0.1"
+
+# Build images
+docker-compose build 
+
+# Tag images
+docker tag ${MYSQL_IMAGE_NAME}:${MYSQL_TAG} ${MYSQL_REPO}
+docker tag ${PHP_IMAGE_NAME}:${PHP_TAG} ${PHP_REPO}
+
+# Push images to the repository
+docker push ${MYSQL_REPO}
+docker push ${PHP_REPO}
+
+# docker push mosharafhossain/ci4-docker-custom-img:tagname
+
