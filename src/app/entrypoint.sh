2303d5155006555108a8a89d51c6dc2ebf03b8c8
mosharaf456 <mosharaf111hossain@gmail.com>
2024-07-05 16:22:54 +0600

    ref entrypoint volume mount issue fixed

--- src/app/entrypoint.sh
+++ src/app/entrypoint.sh
@@ -12,7 +12,7 @@ echo ""
 # Create necessary directories if they don't exist
 mkdir -p /var/www/html/app/logs-cus
 chown -R www-data:www-data /var/www/html/app/logs-cus
-chmod -R 775 /var/www/html/app/logs-cus
+chmod -R 755 /var/www/html/app/logs-cus
 
 echo "TEST sucessful" >> /var/www/html/app/logs-cus/log.php
 
